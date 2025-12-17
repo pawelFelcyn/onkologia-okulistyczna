c76a80d0d389384e2759d2797141ec5c93f0ab37
Pawel Felcyn <pawel.felcyn@onet.pl>
2025-12-17 20:21:24 +0100

    enable continue last unet training

--- cluster_scripts/test_unet.sh
+++ cluster_scripts/test_unet.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=train_unet_model
+#SBATCH --job-name=test_unet_model
 #SBATCH --partition=gpu_spot
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=16:00:00
-#SBATCH --output=unet_train_task-%j.out
+#SBATCH --time=00:30:00
+#SBATCH --output=unet_test_task-%j.out
 #SBATCH --mail-type=ALL
 #SBATCH --mem=16G
 
@@ -12,4 +12,4 @@ module load anaconda
 conda activate nn_train
 cd /projects/onkokul/onkologia-okulistyczna || exit -1
 
-srun python train_model/train_unet.py
+srun python train_model/test_unet.py
