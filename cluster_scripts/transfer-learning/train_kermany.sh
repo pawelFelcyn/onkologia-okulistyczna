91f125b4f13a8ba4cb12f44d1c117d46ee1f8270
Qumpell <qqumpel@gmail.com>
2026-03-07 22:36:30 +0100

    add kermany cluster scripts

---
+++ cluster_scripts/transfer-learning/train_kermany.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=train_kermany_model
+#SBATCH --partition=gpu_spot
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=16:00:00
+#SBATCH --output=train_kermany_task-%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mem=16G
+
+module load anaconda
+conda activate nn_train
+cd /projects/onkokul/onkologia-okulistyczna || exit -1
+
+srun python train_model/transfer_learning/train_kermany.py \
+  --data_dir   train_model/transfer_learning/OCT2018 \
+  --epochs     25 \
+  --batch_size 8 \
+  --seed       42 \
+  --output_dir train_model/transfer_learning/runs_kermany_seed42
