3b3cd34cb03a80f7290ac60a3a4005c403c27c23
Qumpell <qqumpel@gmail.com>
2026-03-20 21:43:57 +0100

    Add second arg to scripts

--- cluster_scripts/transfer-learning/train_unet_transfer_kermany_freeze.sh
+++ cluster_scripts/transfer-learning/train_unet_transfer_kermany_freeze.sh
@@ -10,19 +10,22 @@
 
 # Usage:
 #   sbatch train_unet_transfer_kermany_freeze.sh 13
-# Optional 2nd arg: encoder weights path
-#   sbatch train_unet_transfer_kermany_freeze.sh 13 train_model/transfer_learning/runs_kermany_seed42/encoder_kermany_pretrained.pth
+# Optional 2nd arg: Kermany seed used to pick pretrained encoder weights (default: 42)
+#   sbatch train_unet_transfer_kermany_freeze.sh 13 42
+# Optional 3rd arg: explicit encoder weights path (overrides 2nd arg)
+#   sbatch train_unet_transfer_kermany_freeze.sh 13 42 train_model/transfer_learning/runs_kermany_seed42/encoder_kermany_pretrained.pth
 
 set -euo pipefail
 
 SEED=${1:-42}
-ENCODER_WEIGHTS=${2:-train_model/transfer_learning/runs_kermany_seed${SEED}/encoder_kermany_pretrained.pth}
+KERMANY_SEED=${2:-42}
+ENCODER_WEIGHTS=${3:-train_model/transfer_learning/runs_kermany_seed${KERMANY_SEED}/encoder_kermany_pretrained.pth}
 
 module load anaconda
 conda activate nn_train
 cd /projects/onkokul/onkologia-okulistyczna || exit -1
 
-echo "[INFO] Kermany transfer + freeze UNet training | seed=${SEED} | encoder=${ENCODER_WEIGHTS}"
+echo "[INFO] Kermany transfer + freeze UNet training | run_seed=${SEED} | kermany_seed=${KERMANY_SEED} | encoder=${ENCODER_WEIGHTS}"
 
 srun python train_model/train_unet.py \
   --train_csv Ophthalmic_Scans/splits/tumor_and_fluid_segmentation_oct/train.csv \
